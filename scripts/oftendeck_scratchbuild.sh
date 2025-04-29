ccbeb75781bb913bf135294304ba932fe924da56
oftenzhan <40285982+oftenzhan@users.noreply.github.com>
2025-04-28 21:38:00 -0700

    Update oftendeck_scratchbuild.sh

--- scripts/oftendeck_scratchbuild.sh
+++ scripts/oftendeck_scratchbuild.sh
@@ -56,21 +56,6 @@ sed -i \
     -e 's/^screen-rotate=.*/screen-rotate=3/' \
     ~/.fbtermrc
 
-echo "=== Autologin for all TTY ==="
-USERNAME="oftendeck"
-
-for tty in {1..6}; do
-    mkdir -p /etc/systemd/system/getty@tty${tty}.service.d
-    cat > /etc/systemd/system/getty@tty${tty}.service.d/override.conf <<EOF
-[Service]
-ExecStart=
-ExecStart=-/sbin/agetty --autologin ${USERNAME} --noclear %I \$TERM
-EOF
-done
-
-echo "=== Reloading systemd with autologin ==="
-systemctl daemon-reload
-
 echo "=== Setting $EDITOR as Emacs ==="
 
 # Add default editor settings to ~/.bashrc
