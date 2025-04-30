0842480a349f3f0cef3791c90fde98447da8988e
oftenzhan <40285982+oftenzhan@users.noreply.github.com>
2025-04-29 20:54:58 -0700

    Update oftendeck_Rev2Revamp.sh

--- scripts/oftendeck_Rev2Revamp.sh
+++ scripts/oftendeck_Rev2Revamp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# Directly modify the file to change orientation from 3 to 0
+echo "=== Modify fim rotation ==="
 sed -i "s/g:_orientation=3/g:_orientation=0/" "$HOME/.oftendeck/scripts/fimgs.sh"
 
+echo "=== Modify fbterm rotation ==="
+sed -i "s/^screen-rotate=.*/screen-rotate=0/" "$HOME/.fbtermrc"
