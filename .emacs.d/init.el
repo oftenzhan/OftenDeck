3176b20522ac65d932feb0139515dd3cefe511c3
oftenzhan <40285982+oftenzhan@users.noreply.github.com>
2025-05-03 09:51:45 -0700

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;;  saves backups and autosave files in `.emacs.d/backups/`
+;;  saves backups & autosave files in `~/.emacs.d/backups/`
 (make-directory "~/.emacs.d/backups/" t)
 (setq backup-directory-alist
       `(("." . "~/.emacs.d/backups/")))
