a5a2b734efaa662ac0caf0bdd2c3965d29d61d65
oftenzhan <40285982+oftenzhan@users.noreply.github.com>
2025-05-04 11:31:44 -0700

    Update oftendeck_SmartiPi.sh

--- scripts/oftendeck_SmartiPi.sh
+++ scripts/oftendeck_SmartiPi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "=== Appending to /boot/cmdline.txt ==="
+echo "=== Appending to /boot/firmware/cmdline.txt ==="
 
 echo -n " loglevel=1 quiet logo.nologo" | sudo tee -a /boot/firmware/cmdline.txt > /dev/null
 
-echo "=== Appending to /boot/config.txt ==="
+echo "=== Appending to /boot/firmware/config.txt ==="
 
 echo -e "\n# Disable rainbow splash screen\ndisable_splash=1" | sudo tee -a /boot/firmware/config.txt > /dev/null
 
