dd04dd654f854f4773e7991a30384d41df5e63d2
oftenzhan <40285982+oftenzhan@users.noreply.github.com>
2025-05-04 18:56:34 -0700

    Update oftendeck_Rev2Revamp.sh

--- scripts/oftendeck_Rev2Revamp.sh
+++ scripts/oftendeck_Rev2Revamp.sh
@@ -8,11 +8,11 @@ echo "=== Modify fbterm rotation ==="
 
 sed -i "s/^screen-rotate=.*/screen-rotate=0/" "$HOME/.fbtermrc"
 
-echo "=== Appending to /boot/cmdline.txt ==="
+echo "=== Appending to /boot/firmware/cmdline.txt ==="
 
 echo "$(cat /boot/firmware/cmdline.txt) loglevel=1 quiet logo.nologo" | sudo tee /boot/firmware/cmdline.txt > /dev/null
 
-echo "=== Appending to /boot/config.txt ==="
+echo "=== Appending to /boot/firmware/config.txt ==="
 
 echo -e "\n# Disable rainbow splash screen\ndisable_splash=1" | sudo tee -a /boot/firmware/config.txt > /dev/null
 
