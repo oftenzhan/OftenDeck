c8e159ece67948a8a1b06555a5157445e2789911
oftenzhan <40285982+oftenzhan@users.noreply.github.com>
2025-05-04 18:55:36 -0700

    Update oftendeck_Rev2Revamp.sh

--- scripts/oftendeck_Rev2Revamp.sh
+++ scripts/oftendeck_Rev2Revamp.sh
@@ -14,8 +14,8 @@ echo "$(cat /boot/firmware/cmdline.txt) loglevel=1 quiet logo.nologo" | sudo tee
 
 echo "=== Appending to /boot/config.txt ==="
 
-echo -e "\n# Disable rainbow splash screen\ndisable_splash=1" | sudo tee -a /boot/config.txt > /dev/null
+echo -e "\n# Disable rainbow splash screen\ndisable_splash=1" | sudo tee -a /boot/firmware/config.txt > /dev/null
 
-echo -e "\n# Remove boot delay (less chance to enter recovery)\nboot_delay=0" | sudo tee -a /boot/config.txt > /dev/null
+echo -e "\n# Remove boot delay (less chance to enter recovery)\nboot_delay=0" | sudo tee -a /boot/firmware/config.txt > /dev/null
 
 echo "Done. Reboot to see the changes."
